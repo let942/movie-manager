7f50b296f0d0a2f7ef2a40d48055dea7f10c6865
soficia <let942@gmail.com>
2023-03-21 11:00:59 -0300

    test: create mock movies

---
+++ tests/unit/model/mocks/movie.model.mock.js
@@ -0,0 +1,33 @@
+const movies = [
+  { 
+    id: 2,
+    name: "O Caminho para El Dorado",
+    rating: 100,
+    source: "Fita cassete",
+    year: 2000,
+    whatchedAt: "2005-01-05T00:00:00.000Z",
+    createdAt: "2022-12-05T00:00:00.000Z",
+    updatedAt: "2022-12-05T00:00:00.000Z"
+  },
+  {
+    id: 9,
+    name: "Avatar",
+    rating: 60,
+    source: "DVD",
+    year: 2009,
+    whatchedAt: "2006-01-05T00:00:00.000Z",
+    createdAt: "2023-01-03T14:00:47.555Z",
+    updatedAt: "2023-01-03T14:00:47.555Z"
+  },
+  {
+    id: 10,
+    name: "O Enfermeiro da Noite",
+    rating: 80,
+    source: "Netfiz",
+    year: 2022,
+    whatchedAt: "2022-11-13T00:00:00.000Z",
+    createdAt: "2023-01-03T14:01:36.526Z",
+    updatedAt: "2023-01-03T14:01:36.526Z"
+  }
+
+]
